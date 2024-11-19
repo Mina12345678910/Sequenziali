559168d3078c4e574d7f4ad79a48a0028497f65d
ospite <ospite@PC19-LIM>
2024-11-19 11:52:23 +0100

    kjjkkjjk

---
+++ src/main/java/com/mycompany/_2024_12_11_4id_test/_2024_12_11_4id_test.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._2024_12_11_4id_test;
+
+/**
+ *
+ * @author ospite
+ */
+public class _2024_12_11_4id_test {
+
+   
+    
+//metodo statico Main eseguito dall' appliazione Java    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    
+    
+}
